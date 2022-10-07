0371a615db6045e726ed32f97ac1a811972c9fba
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-10-07 12:38:14 -0400

    Create test_app.py

---
+++ test/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+from src import App
+
+@pytest.fixture()
+def app():
+    app = App.return_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
