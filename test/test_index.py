76da4106ab297862f63d77fb93be1169af7a645d
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-10-07 18:33:50 -0400

    Update test_index.py

--- test/test_index.py
+++ test/test_index.py
@@ -3,7 +3,7 @@ from test import client
 def test_landing(client):
     landing = client.get("/")
     assert landing.status_code == 200
-    assert "Copyright © 2022 - All rights Reserved - Designed by SE-Team11" in landing.data.decode()
+    assert "Copyright © 2022 - All rights Reserved - Designed by SE-Team15" in landing.data.decode()
 
 def test_landing_aliases(client):
     landing = client.get("/")
