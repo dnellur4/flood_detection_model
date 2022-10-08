a8326501f85e1696ef3359847efc2bc50441ca72
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-10-08 17:07:28 -0400

    Update modelprediction.py

--- src/Application/modelprediction.py
+++ src/Application/modelprediction.py
@@ -9,27 +9,28 @@ class ModelPrediction():
         svm_model = None
 
     def get_model(self):
-        '''filename = 'deep_model.sav'
+        filename = 'deep_model.sav'
         if not os.path.exists(filename):
-            os.system("python3 -m gdown 1Vd1TV-MFHqC4IWbJCGcmKreCf5Jx_au_")
+            os.system("gdown 1Vd1TV-MFHqC4IWbJCGcmKreCf5Jx_au_")
         loaded_model = pickle.load(open(filename, 'rb'))
 
         #loading the model
         filename = 'svm_model.sav'
         if not os.path.exists(filename):
-            os.system("python3 -m gdown  1lg3_Ni8r5p1CK9W3DAPsJuM1lyNt8U4a")
-        svm_model = pickle.load(open(filename, 'rb'))'''
-
-        filename = '/Users/VKANAMA/Downloads/deep_model.sav'
+            os.system("gdown  1lg3_Ni8r5p1CK9W3DAPsJuM1lyNt8U4a")
+        svm_model = pickle.load(open(filename, 'rb'))
+        
+        #filename = '/Users/VKANAMA/Downloads/deep_model.sav'
         '''if not os.path.exists(filename):
             print(1)
             os.system("gdown 1Vd1TV-MFHqC4IWbJCGcmKreCf5Jx_au_")'''
-        loaded_model = pickle.load(open(filename, 'rb'))
+        #loaded_model = pickle.load(open(filename, 'rb'))
 
         #loading the model
-        filename = '/Users/VKANAMA/Downloads/svm_model.sav'
+        #filename = '/Users/VKANAMA/Downloads/svm_model.sav'
         '''if not os.path.exists(filename):
             os.system("gdown  1lg3_Ni8r5p1CK9W3DAPsJuM1lyNt8U4a")'''
+        
         self.svm_model = pickle.load(open(filename, 'rb'))
         return loaded_model,self.svm_model
 
