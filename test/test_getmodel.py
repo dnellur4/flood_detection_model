e707bcbeb94cb5a576a9c88874457eeffdb602d1
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-10-08 13:24:18 -0400

    Update test_getmodel.py

--- test/test_getmodel.py
+++ test/test_getmodel.py
@@ -1,9 +1,8 @@
 import pytest
-from src import App 
-from src import main_model
+from src.App import * 
+from src.main_model import *
 class test_getmodel:
   def test_get_model_app(self):
-    if App.get_model():
-      return True
+      return App.get_model()
   def test_get_model_main(self):
     assert main_model.get_model()
