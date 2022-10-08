94c2e0cb1a4c83292cb2bd8ac97c1ce6bbcd9772
Ratan Immidisetti <ratanimmi@gmail.com>
2022-10-08 16:24:00 -0400

    Delete test directory

--- test/test_sample.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src import main_model 
-
-def test_predict_flood():
-  assert main_model.predict_flood('There is a flood.', 'Reddit') == 1
-  assert main_model.predict_flood('All the streets of the city are filled with flood water', 'Twitter') == 1
-  assert main_model.predict_flood('The agricultural lands in are filled with large volumes of rain water.', 'Instagram') == 0
