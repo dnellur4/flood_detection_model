dee429006d2c67b172c77144caf8f864459ee8bc
svengal03 <112122632+svengal03@users.noreply.github.com>
2022-10-08 16:15:18 -0400

    Add files via upload

---
+++ test/test_predict.py
@@ -0,0 +1,10 @@
+from test import client
+def test_predict(client):
+    landing = client.get("/predict")
+    assert landing.status_code == 200
+    # Spot check important text
+    assert "Copyright © 2022 - All rights Reserved - Designed by SE-Team15" in landing.data.decode()
+
+def test_predict_aliases(client):
+    landing = client.get("/predict")
+    assert client.get("/predict/").data 
