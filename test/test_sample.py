ad1f5bca8e732fff5412dd64a3918569be23aee4
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-10-08 13:19:22 -0400

    Update test_sample.py

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,10 +1,8 @@
 import pytest
-from src import main_model 
+from src.main_model import *
 class Predict:
     def test_predict_flood(self):
-      assert main_model.predict_flood('There is a flood.', 'Reddit') == 1
-      assert main_model.predict_flood('All the streets of the city are filled with flood water', 'Twitter') == 1
-
+      assert predict_flood('There is a flood.', 'Reddit') == 1
+    
     def test_predict_no_flood(self):
-      assert main_model.predict_flood('The agricultural lands in are filled with large volumes of rain water.', 'Instagram') == 0
-      assert main_model.predict_flood('sea has water', 'Instagram') == 0
+      assert predict_flood('The agricultural lands in are filled with large volumes of rain water.', 'Instagram') == 0
