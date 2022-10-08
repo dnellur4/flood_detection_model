7fbcf6be82ec54a4ca79bec13303a8ccc2b1a6d7
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-10-08 13:39:33 -0400

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -4,6 +4,6 @@ from src import main_model
 class test_main():
   def test_predict_main(self):
     d,t =  main_model.main()
-    assert isinstance(d,string)
-    assert isinstance(t,string)
+    assert isinstance(d,str)
+    assert isinstance(t,str)
   
